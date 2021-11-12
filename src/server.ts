5ca8cd501c5749443fd65841df4201a68befc51e
Gabriel Pires <gabpiresdev@gmail.com>
2021-11-11 22:00:51 -0300

    Instalando typeorm para iniciar conexão com banco

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.get('/dory', (req, res) => {
+  return res.send('Reconectando vidas');
+});
+
 app.listen(3000, () => console.log('Server is running'));
