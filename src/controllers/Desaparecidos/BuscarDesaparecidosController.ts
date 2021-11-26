c7611995c511f44dd37e13086f8f186d48e77ff8
Gabriel Pires <gabpiresdev@gmail.com>
2021-11-25 21:54:02 -0300

    Correção busca de desaparecidos

--- src/controllers/Desaparecidos/BuscarDesaparecidosController.ts
+++ src/controllers/Desaparecidos/BuscarDesaparecidosController.ts
@@ -2,14 +2,12 @@ import { Response } from 'express';
 import { BuscarDesaparecidosService } from '../../services/Desaparecidos/BuscarDesaparecidosService';
 
 class BuscarDesaparecidosController {
-  async handle(response: Response) {
+  async handle(_, response: Response) {
     const buscarDesaparecidoService = new BuscarDesaparecidosService();
 
     const desaparecidos = await buscarDesaparecidoService.execute();
 
-    console.log(desaparecidos);
-
-    return response.send(desaparecidos);
+    return response.json(desaparecidos);
   }
 }
 
